5856c03b350e6f802f1afebb9fe7489331377609
saarrr-alves <143961014+saarrr-alves@users.noreply.github.com>
2025-03-29 17:15:28 -0300

    Update dados.js

--- js/dados.js
+++ js/dados.js
@@ -1,1 +1,20 @@
-
+let dados = [
+    {
+        titulo: "Midoriya Izuku",
+        descricao: "Izuku Midoriya, mais conhecido como Deku, é o protagonista de My Hero Academia. Nascido sem um Quirk em um mundo onde quase todos os possuem, Deku sonha em se tornar um herói. Após um encontro com seu ídolo, All Might, ele recebe o One For All e inicia sua jornada para se tornar um dos maiores heróis da história.",
+        link: "https://myheroacademia.fandom.com/pt-br/wiki/Izuku_Midoriya",
+        tags: "aluno ua classe 1A protagonista herói sem quirk trio de ouro"
+    }, 
+    {
+        titulo : "Bakugou Katuski", 
+        descricao : "Katsuki Bakugou, também conhecido como Kacchan e pelo nome de herói Great Explosion Murder God Dynamight, é um dos personagens mais populares e complexos de My Hero Academia. Inicialmente apresentado como um valentão arrogante e explosivo, Bakugou revela camadas mais profundas ao longo da série, demonstrando um forte senso de justiça e uma determinação incansável para se tornar o melhor herói. Suas explosões, fruto de seu Quirk, são uma metáfora para sua personalidade intensa e apaixonada, e sua jornada de autodescoberta e crescimento o tornam um personagem fascinante para os fãs.", 
+        link : "https://myheroacademia.fandom.com/pt-br/wiki/Katsuki_Bakugo", 
+        tags: "aluno ua classe 1A principal herói trio de ouro"
+    }, 
+    {
+        titulo: "Todoroki Shoto", 
+        descricao: "Shoto Todoroki é um dos alunos mais promissores da U.A. High School. Filho de um dos heróis mais poderosos, Endeavor, Todoroki possui um Quirk que lhe permite controlar o gelo e o fogo. No entanto, devido ao passado traumático com seu pai, ele luta para usar seu poder em todo o seu potencial. Ao longo da série, ele busca encontrar seu próprio caminho como herói.", 
+        link: "https://myheroacademia.fandom.com/pt-br/wiki/Shoto_Todoroki", 
+        tags: "aluno ua classe 1A principal herói trio de ouro"
+    }
+]
